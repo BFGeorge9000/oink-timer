ed09d7a06d5f93de199d66239a22bcf21e37a592
George Milberger <georgemilberger@hotmail.com>
2013-05-17 16:54:45 -0400

    slider logic

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -2,6 +2,7 @@ class Timer < ActiveRecord::Base
   attr_accessible :duration, :name
 
   MAX_DURATION = 30.minutes
+  MIN_DURATION = 15.minutes
   REMAINING_TIME_FUNCTION = "(created_at + (duration * interval '1 second'))"
 
   validates_numericality_of :duration, :greater_than => 0, :less_than_or_equal_to => MAX_DURATION
@@ -9,6 +10,10 @@ class Timer < ActiveRecord::Base
 
   scope :active, where("#{REMAINING_TIME_FUNCTION} > ?", Time.now)
   scope :remaining_ascending, order("#{REMAINING_TIME_FUNCTION} ASC")
+
+  def self.default_duration
+    MIN_DURATION + ((MAX_DURATION - MIN_DURATION) / 2.to_f)
+  end
 
   def remaining_duration
     remaining = (duration - (Time.now - created_at)).to_i
