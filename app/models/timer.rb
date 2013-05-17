5281575e8041e0760431894b7c159f02e7cb4f72
George Milberger <georgemilberger@hotmail.com>
2013-05-17 15:55:14 -0400

    Make timer model smarter

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,7 +1,13 @@
 class Timer < ActiveRecord::Base
   attr_accessible :duration, :name
 
-  validates_numericality_of :duration, :greater_than => 0
+  MAX_DURATION = 30.minutes
+
+  validates_numericality_of :duration, :greater_than => 0, :less_than_or_equal_to => MAX_DURATION
   validates_presence_of :name, :duration
 
+  def remaining_duration
+    remaining = (duration - (Time.now - created_at)).to_i
+    remaining >= 0 ? remaining : 0
+  end
 end
