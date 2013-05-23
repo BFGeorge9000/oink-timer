9723033c8e451ead3f3cb0d24c3cbdec58086469
George Milberger <georgemilberger@hotmail.com>
2013-05-23 11:05:41 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BaconTimer::Application.routes.draw do
-  root :to => "timers#index"
+  # root :to => "timers#index"
+  root :to => "timers#welcome"
   resources :timers, :except => [:edit, :update, :destroy, :show]
 end
