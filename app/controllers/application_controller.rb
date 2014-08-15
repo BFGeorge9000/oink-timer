4c6002809b354b1566e038547bd164b533a21d97
BFGeorge9000 <georgemilberger@hotmail.com>
2014-08-15 03:16:44 -0400

    Roughed-in Google Analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_analytics_token
+
+  private
+
+  def set_analytics_token
+    @analytics_token ||= YAML.load_file("#{::Rails.root.to_s}/config/google_analytics.yml")[:token]
+  end
+
 end
